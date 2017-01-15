68573330cd69ddb81781fd1c01a70f8ac7f3a79a
Tiago Martinho <tiago@elit.software>
2017-01-15 13:21:48 +0100

    Define emoji UI that updates with Taplytics

--- ABTestingSpike/ViewController.swift
+++ ABTestingSpike/ViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Taplytics
 
 class ViewController: UIViewController {
 
@@ -6,4 +7,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var neutral: UIImageView!
     @IBOutlet weak var sad: UIImageView!
 
+    var shouldUseEmojis: TaplyticsVar?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        initTaplytics()
+    }
+
+    func initTaplytics() {
+        let taplyticsVarName = "shouldUseEmojis"
+        let defaultValue = false as NSObject
+        shouldUseEmojis = TaplyticsVar(name: taplyticsVarName,
+                                       defaultValue: defaultValue,
+                                       updatedBlock: updatedBlock)
+    }
+
+    func updatedBlock(updatedValue: NSObject?) {
+        updateUI()
+    }
+
+    func updateUI() {
+        let shouldUseEmojis = self.shouldUseEmojis?.value as? Bool == true
+
+        happy.image = shouldUseEmojis ? #imageLiteral(resourceName: "happy-emoji") : #imageLiteral(resourceName: "happy")
+        neutral.image = shouldUseEmojis ? #imageLiteral(resourceName: "neutral-emoji") : #imageLiteral(resourceName: "neutral")
+        sad.image = shouldUseEmojis ? #imageLiteral(resourceName: "sad-emoji") : #imageLiteral(resourceName: "sad")
+    }
 }
